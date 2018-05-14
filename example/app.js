57884284df95596db40e666b2b98166648f7da49
KedAyAyA <caoxun333@gmail.com>
2018-05-14 11:47:51 +0800

    add: simple example

---
+++ example/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+console.log(path.resolve(__dirname, '../dist/ktimer.js'), path.resolve(__dirname, './public/js/ktimer.js'))
+
+fs.copyFileSync(path.resolve(__dirname, '../dist/ktimer.js'), path.resolve(__dirname, './public/js/ktimer.js'))
+
+app.use(express.static(path.resolve(__dirname, './public')))
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html')
+})
+
+app.listen(10099, () => console.log('Example server start on port 10099'))
